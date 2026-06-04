e04b21049d74ef5004f12a1d598f8b606660a535
ahshanulsoftvence <ahshanul@softvence.com>
2026-06-04 15:13:51 +0600

    Updated folder structure

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,7 +4,7 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-slate-50">
+    <main className="min-h-screen flex items-center justify-center text-white">
       {children}
     </main>
   );
