08472c35a914e6006a8cbff5844549f9132d540b
ahshanulsoftvence <ahshanul@softvence.com>
2026-06-17 12:16:45 +0600

    Integrated api in toggle bookmark in law card

--- src/app/(protected)/(user)/bookmarks/page.tsx
+++ src/app/(protected)/(user)/bookmarks/page.tsx
@@ -7,9 +7,11 @@ import { lawsDataset } from "@/components/user/dashboard/LawsAndBylaws";
 import { hearingsDataset } from "@/components/user/dashboard/UpcomingHearings";
 import { useGetAllBookmarkedCasesQuery } from "@/store/features/case/case.api";
 import { useGetAllBookmarkedLawsQuery } from "@/store/features/lawAndBylaw/lawAndBylaw.api";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Page() {
+    const navigate = useRouter()
     const {data: bookmarkedCases, isLoading: isBookmarkedCasesLoading} = useGetAllBookmarkedCasesQuery();
     const {data: bookmarkedLaws, isLoading: isBookmarkedLawsLoading} = useGetAllBookmarkedLawsQuery();
     const [activeBtn, setActiveBtn] = useState("cases");
@@ -27,7 +29,8 @@ export default function Page() {
                             onClick={() => setActiveBtn("law")}
                             className={`${activeBtn === "law" ? "bg-[#135576] text-white" : "bg-[#135576]/10 text-[#135576]"} w-full md:w-auto  px-5 py-2 rounded-full hover:cursor-pointer transition-all duration-300`}>Law & ByLaw</button>
                     </div>
-                    <button className="w-full md:w-auto border border-black/50 hover:bg-red-500 hover:text-white hover:border-red-500 px-3 py-2 rounded-full hover:cursor-pointer transition-all duration-300">
+                    <button onClick={()=> navigate.push("/")}
+                    className="w-full md:w-auto border border-black/50 hover:bg-red-500 hover:text-white hover:border-red-500 px-3 py-2 rounded-full hover:cursor-pointer transition-all duration-300">
                         Clear all
                     </button>
                 </div>
@@ -44,9 +47,10 @@ export default function Page() {
                                 <LawCard
                                     key={index}
                                     title={law.title}
-                                    category={law.category}
-                                    officialGazette={law.officialGazette}
-                                    lastUpdate={law.lastUpdate}
+                                    category={law.category_name}
+                                    officialGazette={law.official_gazette}
+                                    lastUpdate={law.last_updated}
+                                    bookmark={law.bookmark}
                                 />
                             ))}
                         </div>
